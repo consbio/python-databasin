299052e27f8017a8156a4156ba2e224466d67bb7
nikmolnar <nik.molnar@consbio.org>
2015-11-17 17:47:21 -0800

    Commit initial files.

---
+++ README.md
@@ -0,0 +1,10 @@
+# python-databasin 0.1.0
+
+[![Build Status](https://travis-ci.org/consbio/python-databasin.png?branch=master)](https://travis-ci.org/consbio/python-databasin)
+
+```python-databasin``` is a client library for interacting with [Data Basin](http://databasin.org).
+
+# Install
+```bash
+$ pip install python-databasin
+```
---
+++ setup.py
@@ -0,0 +1,35 @@
+import subprocess
+
+import sys
+from setuptools import Command, setup
+
+import databasin
+
+
+class PyTest(Command):
+    user_options = []
+
+    def initialize_options(self):
+        pass
+
+    def finalize_options(self):
+        pass
+
+    def run(self):
+        errno = subprocess.call([sys.executable, 'runtests.py'])
+        raise SystemExit(errno)
+
+
+setup(
+    name='python-databasin',
+    description='A client library for Data Basin (http://databasin.org)',
+    author='Conservation Biology Institute',
+    keywords='data basin',
+    version=databasin.__version__,
+    packages=['databasin'],
+    requires=['six', 'requests', 'restle'],
+    url='https://github.com/consbio/python-databasin',
+    license='BSD',
+    tests_require=['pytest', 'requests-mock', 'mock'],
+    cmdclass={'test': PyTest}
+)
